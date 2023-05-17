924600d490e5061b0e62922be2e8d876517ad35c
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2023-05-17 21:22:21 +0200

    prova

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,14 @@ import Image from 'next/image'
 import jungle from '@img/jungle.png'
 import useStore from "@store";
 import '@/styles/globals.css'
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
 
   const inizializeAdjs = useStore((state) => state.inizializeAdjs);
   const inizializeAnimals = useStore((state) => state.inizializeAnimals);
   let inizialize = false;
-  const r = useRouter();
 
   useEffect(()=>{
-    console.log(r.pathname);
     if(!inizialize){
       inizialize=true;
       console.log(inizialize);
